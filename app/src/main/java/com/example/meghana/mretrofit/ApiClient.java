b1feb74015d84e493609a350222c9f0550cff0c9
Hmeghana <meghanababu1995@gmail.com>
2018-10-31 17:20:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/meghana/mretrofit/ApiClient.java
@@ -0,0 +1,4 @@
+package com.example.meghana.mretrofit;
+
+public class ApiClient {
+}
---
+++ app/src/main/java/com/example/meghana/mretrofit/Model.java
@@ -0,0 +1,43 @@
+package com.example.meghana.mretrofit;
+
+public class Model {
+    public Model() {
+    }
+
+
+
+    private String constiuency;
+
+    public Model(String constiuency, String MLA, String party) {
+        this.constiuency = constiuency;
+        this.MLA = MLA;
+        this.party = party;
+    }
+
+    public String getConstiuency() {
+        return constiuency;
+    }
+
+    public void setConstiuency(String constiuency) {
+        this.constiuency = constiuency;
+    }
+
+    public String getMLA() {
+        return MLA;
+    }
+
+    public void setMLA(String MLA) {
+        this.MLA = MLA;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
+    public void setParty(String party) {
+        this.party = party;
+    }
+
+    private String MLA;
+    private String party;
+}
